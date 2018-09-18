11005811ea2e4a2e958a6f5e3be1b825e3b3d633
Igor Borges <igor@borges.me>
2018-09-18 20:27:58 -0300

    ifood_party_bot in dart, from stagehand

---
+++ ifood_party_bot/bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:ifood_party_bot/ifood_party_bot.dart' as ifood_party_bot;
+
+main(List<String> arguments) {
+  print('Hello world: ${ifood_party_bot.calculate()}!');
+}
---
+++ ifood_party_bot/test/ifood_party_bot_test.dart
@@ -0,0 +1,8 @@
+import 'package:ifood_party_bot/ifood_party_bot.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
